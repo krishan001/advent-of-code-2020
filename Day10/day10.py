250e9222b9c92a7a1078678d946125509cc3c987
Krishan Patel <sc17kdp@leeds.ac.uk>
2020-12-14 17:50:13 +0000

    day10 completed, have got a few days behind

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,3 +1,5 @@
+from itertools import chain, combinations
+
 
 def get_sorted_list(adapters):
     adapters.append(0)
@@ -6,12 +8,41 @@ def get_sorted_list(adapters):
     return adapters
 
 
+def get_differences(sorted_list):
+    return [x - sorted_list[i - 1] for i, x in enumerate(sorted_list)][1:]
+
+
+def get_next_items(target, sorted_list):
+    next_items = []
+    for i in range(1,4):
+        item = target + i
+        if item in sorted_list:
+            next_items.append(item)
+    return next_items
+
+
+def count_combinations(sorted_list):
+    dp = [1]
+    for i in range(1, len(sorted_list)):
+        ans = 0
+        for j in range(0, i):
+            if sorted_list[j] + 3 >= sorted_list[i]:
+                ans += dp[j]
+        dp.append(ans)
+    return dp[-1]
+
+
 def part1(adapters):
     """ Get the combination of adapters to connect your device and the differences between them"""
     sorted_list = get_sorted_list(adapters)
-    differences = [x - sorted_list[i - 1] for i, x in enumerate(sorted_list)][1:]
+    differences = get_differences(sorted_list)
     return differences.count(1) * differences.count(3)
 
 
+def part2(adapters):
+    total_combinations = 1
+    sorted_list = get_sorted_list(adapters)
+    return count_combinations(sorted_list)
 
-    
+
+
--- Day10/day10_test.py
+++ Day10/day10_test.py
@@ -9,6 +9,8 @@ def test_day10_part_1():
     number_list = [int(i) for i in test_input_data]
     assert day10.part1(number_list) == 220
 
-# def test_day10_part_2():
-#     number_list = [int(i) for i in test_input_data]
-#     assert day10.part2(number_list) == 19208
+def test_day10_part_2():
+    number_list = [int(i) for i in test_input_data]
+    assert day10.part2(number_list) == 19208
+    number_list2 = [int(i) for i in input_data]
+    assert day10.part2(number_list2) == 13816758796288
